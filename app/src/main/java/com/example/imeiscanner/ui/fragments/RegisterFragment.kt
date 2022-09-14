fec3ae51e01b0f0cb7255a8d8a58bc7ddce8ec12
Hp <saidisxoq@gmail.com>
2022-09-14 15:21:18 +0300

    register fragment created!

---
+++ app/src/main/java/com/example/imeiscanner/ui/fragments/RegisterFragment.kt
@@ -0,0 +1,26 @@
+package com.example.imeiscanner.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.imeiscanner.R
+import com.example.imeiscanner.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+}
