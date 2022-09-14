b137624a2e2c8a77dd66a95b32d69af52f5fae39
yunus <teslayui@gmail.com>
2022-09-14 13:40:22 +0300

    enter code adding

---
+++ app/src/main/java/com/example/imeiscanner/ui/fragments/EnterCodeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.imeiscanner.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.imeiscanner.R
+
+
+class EnterCodeFragment : Fragment() {
+
+}
