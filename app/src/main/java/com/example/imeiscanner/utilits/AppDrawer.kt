a9a6c8b0d2270be0acce3936c5cb0dd51df45d7b
Hp <saidisxoq@gmail.com>
2022-09-15 16:41:51 +0300

    RegisterFragment.kt phone number is finished!

---
+++ app/src/main/java/com/example/imeiscanner/utilits/AppDrawer.kt
@@ -0,0 +1,15 @@
+package com.example.imeiscanner.utilits
+
+import androidx.drawerlayout.widget.DrawerLayout
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+
+class AppDrawer {
+
+    private lateinit var mDrawer: AppDrawer
+    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mCurrentProfile: ProfileDrawerItem
+
+
+}
