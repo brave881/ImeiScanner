957baee2e74a0ba0890d186b50b663a415e85aa1
Hp <saidisxoq@gmail.com>
2022-09-15 18:11:52 +0300

    RegisterFragment.kt phone number is finished! 111

--- app/src/main/java/com/example/imeiscanner/utilits/AppDrawer.kt
+++ app/src/main/java/com/example/imeiscanner/utilits/AppDrawer.kt
@@ -2,14 +2,35 @@ package com.example.imeiscanner.utilits
 
 import androidx.drawerlayout.widget.DrawerLayout
 import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class AppDrawer {
 
-    private lateinit var mDrawer: AppDrawer
+    private lateinit var mDrawer: Drawer
     private lateinit var mDrawerLayout: DrawerLayout
     private lateinit var mHeader: AccountHeader
     private lateinit var mCurrentProfile: ProfileDrawerItem
 
+    fun create(){
+        initLoader()
+        createHeader()
+        createDrawer()
+        mDrawerLayout=mDrawer.drawerLayout
+    }
+
+    private fun createDrawer() {
+
+
+    }
+
+
+    private fun createHeader() {
+
+    }
+
+    private fun initLoader() {
+
+    }
 
 }
