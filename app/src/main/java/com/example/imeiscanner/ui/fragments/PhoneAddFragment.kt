01a0206577e777b93578cd5aa7d1105261ed60e1
broth <teslayui@gmail.com>
2022-09-20 11:43:57 +0300

    Calendar view

--- app/src/main/java/com/example/imeiscanner/ui/fragments/PhoneAddFragment.kt
+++ app/src/main/java/com/example/imeiscanner/ui/fragments/PhoneAddFragment.kt
@@ -1,7 +1,6 @@
 package com.example.imeiscanner.ui.fragments
 
 import android.annotation.SuppressLint
-import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -9,13 +8,20 @@ import android.view.View
 import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.EditText
+import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
 import com.example.imeiscanner.databinding.FragmentPhoneAddBinding
-import java.util.*
+import com.example.imeiscanner.utilits.QrScanner
+import com.example.imeiscanner.utilits.showDatePicker
+import com.example.imeiscanner.utilits.showToast
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 
 class PhoneAddFragment : Fragment() {
     private lateinit var binding: FragmentPhoneAddBinding
+    private lateinit var qrScanner: QrScanner
+    private lateinit var options: ScanOptions
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -26,25 +32,18 @@ class PhoneAddFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        binding.btnDateUse.setOnClickListener { showDatePicker() }
+        binding.btnDateUse.setOnClickListener { showDatePicker(requireContext(), binding) }
+        qrScanner = QrScanner()
+        initFunctions()
+
+    }
+
+
+    fun initFunctions(){
+        scanOptions()
     }
 
     @SuppressLint("SetTextI18n")
-    private fun showDatePicker() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        val datePickerDialog = DatePickerDialog(
-            requireContext(), { _, year1, month1, dayOfMonth ->
-                binding.btnDateUse.text = "$dayOfMonth/${month1 + 1}/$year1"
-            }, year, month, day
-        )
-        datePickerDialog.show()
-    }
-
-
     private fun qrScan(editText: EditText) {
         val DRAWABLE_RIGHT = 2
         editText.also {
@@ -52,16 +51,37 @@ class PhoneAddFragment : Fragment() {
                 override fun onTouch(v: View, event: MotionEvent): Boolean {
                     if (event.action == MotionEvent.ACTION_UP) {
                         if (event.rawX >= it.right - it.compoundDrawables[DRAWABLE_RIGHT].bounds.width()) {
+                            // start Scanner
+                            val barcodeLauncher: ActivityResultLauncher<ScanOptions> =
+                                registerForActivityResult(
+                                    ScanContract()
+                                ) { result ->
+                                    if (result.contents == null) {
+                                        showToast("Cancelled")
+                                    } else {
+                                        showToast("Scanned ${result.contents}")
+                                    }
+                                }
 
-                            // your action here
+                            barcodeLauncher.launch(options)
+
+                            // end scanner
                             return true
                         }
                     }
                     return false
-
                 }
             })
         }
+    }
+
+    private fun scanOptions() {
+        options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.ONE_D_CODE_TYPES)
+        options.setPrompt("Scan a barcode")
+        options.setCameraId(0) // Use a specific camera of the device
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
     }
 
 }
---
+++ app/src/main/java/com/example/imeiscanner/utilits/QrScanner.kt
@@ -0,0 +1,9 @@
+package com.example.imeiscanner.utilits
+
+import androidx.activity.result.ActivityResultLauncher
+import com.example.imeiscanner.databinding.FragmentPhoneAddBinding
+import com.journeyapps.barcodescanner.ScanOptions
+
+class QrScanner {
+
+}
