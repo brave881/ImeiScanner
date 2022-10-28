46e927decd4360bb173e36dc09480daeb67b9e05
Hp <saidisxoq@gmail.com>
2022-10-28 17:37:58 +0300

    new new new

--- app/src/main/java/com/example/imeiscanner/models/UserModel.kt
+++ app/src/main/java/com/example/imeiscanner/models/UserModel.kt
@@ -4,7 +4,7 @@ data class UserModel(
     var fullname: String = "",
     var phone: String = "",
     var email: String = "",
-    var photoUrl: String = "empty",
+    var photoUrl: String = "https://firebasestorage.googleapis.com/v0/b/imei-scanner-d7ab3.appspot.com/o/teahub.io-beautiful-wallpaper-hd-for-570952.jpg?alt=media&token=ad233afa-04bd-42f9-ac9b-5408e3387fe0",
     var type: String = ""
 )
 
